18e2c5fb639f2684c9e5bdf46b222c9d4bd04010
mesbahmajed <30556417+mesbahmajed@users.noreply.github.com>
2018-02-22 16:23:37 +0200

    Create api.py file

---
+++ api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+import socket
+
+
+app = Flask(__name__)
+api = Api(app)
+method = socket.gethostname()
+@app.route('/')
+def api_root():
+    return 'Welcome'
+
+
+@app.route('/hello/<nameid>')
+def api_name(nameid):
+
+    names = {'john', 'steve', 'bill'}
+
+
+    if nameid in names:
+        return jsonify({'message': 'Hello ' + nameid + ' from ' + method})
+    else:
+        return  jsonify({'error' : 'Name is not correct'})
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0',port=5000)
