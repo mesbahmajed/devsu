aec8d10b5bbbffcf3094a2cba2e81c23d7bbfa69
mesbahmajed <30556417+mesbahmajed@users.noreply.github.com>
2018-02-22 20:17:41 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import unittest
+import requests
+import json
+import sys
+import socket
+import api
+
+firstarg = sys.argv.pop()
+
+method = socket.gethostname()
+
+class TestFlaskApiUsingRequests(unittest.TestCase):
+    def test_hello_world(self):
+        response = requests.get('http://0.0.0.0:5000')
+        self.assertEqual(response.json(), {'hello': 'world'})
+
+
+class TestFlaskApi(unittest.TestCase):
+
+    firstarg = ""
+
+    def setUp(self):
+        self.app = api.app.test_client()
+
+    def test_hello_world(self):
+        response = self.app.get('/hello/'+firstarg)
+        self.assertEqual(json.loads(response.get_data()),{'message': 'Hello ' + firstarg + ' from ' + method })
+
+
+
+
+if __name__ == "__main__":
+
+        unittest.main()
