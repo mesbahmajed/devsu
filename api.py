70ad6b280080d0c824b4e25288a4715da5ba13d9
mesbahmajed <30556417+mesbahmajed@users.noreply.github.com>
2018-02-22 16:28:27 +0200

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
-from flask.ext.jsonpify import jsonify
+from flask_jsonpify import jsonify
 import socket
 
 
